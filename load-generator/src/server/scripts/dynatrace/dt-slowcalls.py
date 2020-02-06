885961adc2636d27b1584e0d9398607c2ff4bbaf
trypoc <60596853+trypoc@users.noreply.github.com>
2020-02-06 07:17:28 +0000

    Configured Dynatrace Scripts

---
+++ load-generator/src/server/scripts/dynatrace/dt-slowcalls.py
@@ -0,0 +1,14 @@
+import os 
+from selenium import webdriver
+chrome_path="C:\\Users\\MF35121T\\AppData\\Local\\Programs\\Python\\Python38-32\\Scripts\\chromedriver.exe"
+driver=webdriver.Chrome(chrome_path)
+driver.maximize_window()
+driver.implicitly_wait(10)
+driver.get('http://10.210.16.8:8780/konakart/Welcom.action')
+driver.implicitly_wait(10)
+for i in range (0,20):    
+	driver.get('http://10.210.16.8:8780/konakart/Games/1_2_-1_-1.action')  
+	driver.implicitly_wait(10)
+	driver.get('http://10.210.16.8:8780/konakart/DVD-Movies/1_3_-1_-1.action')    
+	driver.implicitly_wait(10)
+driver.quit()
