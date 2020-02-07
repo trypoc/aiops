48629a8a6c4abd90eb917a0bdcd971f336958c84
Paramasivam .K <param.others@gmail.com>
2020-02-07 15:49:23 +0530

    Build files added

---
+++ load-generator/build/precache-manifest.14343ac3a7d17046989e66d619d8b075.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197136690180f2445889cdde18e3af73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfc1210d727ba13b0eef",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "3bae39a295a96763f744",
+    "url": "/static/css/main.27284af2.chunk.css"
+  },
+  {
+    "revision": "cfc1210d727ba13b0eef",
+    "url": "/static/js/2.f4233bf8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f4233bf8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3bae39a295a96763f744",
+    "url": "/static/js/main.91d0b9d8.chunk.js"
+  },
+  {
+    "revision": "3d6b8b8919c63f080bfa",
+    "url": "/static/js/runtime-main.b6fd7294.js"
+  }
+]);
