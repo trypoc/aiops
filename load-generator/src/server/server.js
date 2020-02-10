0cff3ee27d31027b763f432ffc94a4f6727664c6
trypoc <60596853+trypoc@users.noreply.github.com>
2020-02-10 15:07:05 +0530

    Updated usecase names

--- load-generator/src/server/server.js
+++ load-generator/src/server/server.js
@@ -1,64 +1,80 @@
 let express = require("express");
 let path = require('path');
+let cors = require('cors');
 let listenerChildProcessEvents = require('./EventListeners.js')
 let app = express();
 
 app.use(express.static('public'))
+app.use(cors());
 
 app.get("/execute", (req, res) => {
 
   console.log("Query String ", req.query);
-//   console.log("Execute given command ", req.query.name);
-//   console.log("Execute given command ", req.query.script);
-  console.log(" req.query.toolName ", req.query.toolName);
+  //   console.log("Execute given command ", req.query.name);
+  //   console.log("Execute given command ", req.query.script);
+  //   console.log("Execute given command ", req.query.type);
 
   const { spawn } = require("child_process");
 
   let childProcess;
-  let pythonExePath = 'E:/param/software/python381/python';
+  let pythonExePath = 'python';
+  //let pythonExePath = 'C:/Users/MF35121T/Downloads/python381/python';
   let commandExePath = 'C:/Windows/System32/cmd.exe';
-  let scriptAbsoultPath = path.join( __dirname, "/public/"+req.query.script);
+  let scriptAbsoultPath = path.join(__dirname, "/scripts/" + req.query.script);
 
   res.set("Content-Type", "text/plain");
 
-  switch(req.query.type){
+  switch (req.query.type) {
 
-    case 'command' :
-        console.log(`Given command-task is '${req.query.script}'`);
-        childProcess = spawn(req.query.script, {shell: true });
-        registerSTDIOStreams(childProcess, res);
-        listenerChildProcessEvents(childProcess);
-        break;
+    case 'command':
+      console.log(`Given command-task is '${req.query.script}'`);
+      childProcess = spawn(req.query.script, { shell: true });
+      registerSTDIOStreams(childProcess, res);
+      listenerChildProcessEvents(childProcess);
+      break;
 
-    case 'python' :
-        console.log(`Given python-task is '${req.query.script}'`);
-        childProcess = spawn(pythonExePath, [scriptAbsoultPath], {shell: true });
-        registerSTDIOStreams(childProcess, res);
-        listenerChildProcessEvents(childProcess);
-        break;
+    case 'python':
+      console.log(`Given python-task is '${req.query.script}'`);
+      childProcess = spawn(pythonExePath, [scriptAbsoultPath], { shell: true });
+      registerSTDIOStreams(childProcess, res);
+      listenerChildProcessEvents(childProcess);
+      break;
 
-    case 'batch' :
-        console.log(`Given batch-task is '${req.query.script}'`);
-        console.log('Bath file path >',scriptAbsoultPath);
-        // childProcess = spawn(commandExePath, ['/c', scriptAbsoultPath], {shell: true });
-        childProcess = spawn(scriptAbsoultPath, {shell: true });
-        registerSTDIOStreams(childProcess, res);
-        listenerChildProcessEvents(childProcess);
-        break;
+    case 'tomcat':
+      console.log(`Given batch-task is '${req.query.script}'`);
+      console.log('Bath file path >', req.query.script);
+      // childProcess = spawn(commandExePath, ['/c', scriptAbsoultPath], {shell: true });
+      childProcess = spawn(req.query.script, {
+        shell: true,
+        env: {
+          CATALINA_HOME: 'C:/KonaKart',
+          JAVA_HOME: 'C:/Program Files/Java/jdk1.8.0_221'
+        }
+      });
+      registerSTDIOStreams(childProcess, res);
+      listenerChildProcessEvents(childProcess);
+      break;
 
+    case 'mysql':
+      console.log(`Given batch-task is '${req.query.script}'`);
+      console.log('Bath file path >', req.query.script);
+      childProcess = spawn(scriptAbsoultPath, {shell: true });      
+      registerSTDIOStreams(childProcess, res);
+      listenerChildProcessEvents(childProcess);
+      break;
     default:
-        console.log('Nothing matched ...')
-        res.end('This is Default Case and Nothing to executed');
-  }  
+      console.log('Nothing matched ...')
+      res.end('This is Default Case and Nothing to executed');
+  }
 });
 
 const registerSTDIOStreams = (childProcess, res) => {
-    
-    childProcess.stdout.pipe(res);
-    childProcess.stderr.pipe(res);
-    // childProcess.stdout.on('data', data => {
-    //     console.log(data.toString())
-    // });
+
+  childProcess.stdout.pipe(res);
+  childProcess.stderr.pipe(res);
+  // childProcess.stdout.on('data', data => {
+  //     console.log(data.toString())
+  // });
 
 }
 
